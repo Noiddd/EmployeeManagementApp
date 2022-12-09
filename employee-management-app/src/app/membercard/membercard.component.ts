c02e22415cfa42c2041a0e987c6ecdd128b7bc3e
Noiddd <dionang11@gmail.com>
2022-12-09 22:45:47 +0800

    team

--- employee-management-app/src/app/membercard/membercard.component.ts
+++ employee-management-app/src/app/membercard/membercard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Employee } from '../employee';
+import { faXmark, faPen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-membercard',
   templateUrl: './membercard.component.html',
-  styleUrls: ['./membercard.component.css']
+  styleUrls: ['./membercard.component.css'],
 })
 export class MembercardComponent {
+  @Input() employee!: Employee;
 
+  faXmark = faXmark;
+  faPen = faPen;
 }
